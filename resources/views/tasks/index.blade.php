2a64ad447e34f540f9749ff11ba511d4198b291e
tkgakuto <s20ah198@seinan-gakuin.jp>
2019-05-03 12:52:14 +0000

    CRUD tasks

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!--ここに書く-->
+
+    <h1>タスク一覧</h1>
+    
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タスク</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($tasks as $task)
+                <tr>
+                    <td>{{ $task->id }}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif    
+    
+    {!! link_to_route('tasks.create', '新規タスクの投稿', null, ['class' => 'btn btn-primary']) 
+    !!}
+
+@endsection
