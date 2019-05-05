26db1fd6e518632339a5f5f75a5595ec51653977
tkgakuto <s20ah198@seinan-gakuin.jp>
2019-05-05 08:42:19 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -45,6 +45,14 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'status'=> 'required|max:10',
+            ]);
+            
+        $this->validate($request, [
+            'content'=>'required',
+            ]);    
+        
         $task = new Task;
         $task->status = $request->status;
         $task->content = $request->content;
@@ -92,6 +100,14 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request,[
+            'status'=>'required|max:10',
+            ]);
+        
+        $this->validate($request, [
+            'content'=>'required',
+            ]);    
+        
         $task = Task::find($id);
         $task->status=$request->status;
         $task->content=$request->content;
